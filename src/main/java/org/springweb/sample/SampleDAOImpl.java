7869e945910492e3bc3a88d277170b9e08ab6648
wonnho71 <wonnho@nate.com>
2026-05-04 20:17:54 +0900

    loose coupling, interface SampleDAO,SampleDAOImpe,EvenSampleDAOImpl

--- src/main/java/org/springweb/sample/SampleDAOImpl.java
+++ src/main/java/org/springweb/sample/SampleDAOImpl.java
@@ -2,6 +2,7 @@ package org.springweb.sample;
 
 import org.springframework.stereotype.Repository;
 
+@Repository
+public class SampleDAOImpl implements SampleDAO {
 
-public interface SampleDAO {
 }
