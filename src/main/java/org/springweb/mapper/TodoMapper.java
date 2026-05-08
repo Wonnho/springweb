57e4483b6f407b64747e52723f8b34f62dc27e37
wonnho71 <wonnho@nate.com>
2026-05-08 19:46:58 +0900

    add List<TodoVO> selectAll() to TodoMapper class

--- src/main/java/org/springweb/mapper/TodoMapper.java
+++ src/main/java/org/springweb/mapper/TodoMapper.java
@@ -1,10 +1,15 @@
 package org.springweb.mapper;
 
 import org.springweb.domain.TodoVO;
+
+import java.util.List;
+
 public interface TodoMapper {
 
     String getTime();
 
     void insert(TodoVO todoVO);
+
+    List<TodoVO> selectAll();
 
 }
