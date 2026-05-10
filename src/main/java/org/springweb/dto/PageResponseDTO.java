442c12ce3dde881fe60952f652e338633c7cd414
wonnho71 <wonnho@nate.com>
2026-05-10 11:49:37 +0900

    count page numbers, present page number

--- src/main/java/org/springweb/dto/PageResponseDTO.java
+++ src/main/java/org/springweb/dto/PageResponseDTO.java
@@ -1,6 +1,8 @@
 package org.springweb.dto;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -18,6 +20,8 @@ private int end;
 private boolean prev;
 private boolean next;
 
+@Getter
+@ToString
 private List<E> dtoList;
 
     @Builder(builderMethodName = "withAll")
@@ -27,6 +31,23 @@ private List<E> dtoList;
 
         this.total=total;
         this.dtoList=dtoList;
+
+        //end page
+        this.end=(int)(Math.ceil(this.page/10.0))*10;
+        //start page
+        this.start=this.end-9;
+
+        // last page
+        int last=(int)(Math.ceil((total/(double)size)));
+
+        //이론상의 끝과 실제 데이터상의 끝을 비교하여, 더 작은 값을 최종 페이지 끝 번호로 선택한다
+        this.end=end>last?last:end;
+
+        this.prev=this.start >1;
+        this.next=total>this.end*this.size;
+
     }
+
+
 
 }
