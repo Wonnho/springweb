15220c5b2c771d597762b52a21027b61a1230981
wonnho71 <wonnho@nate.com>
2026-05-12 13:14:37 +0900

    deal with link at modify.jsp; page moves when deleting/modifying

--- src/main/java/org/springweb/controller/TodoController.java
+++ src/main/java/org/springweb/controller/TodoController.java
@@ -91,29 +91,39 @@ public class TodoController {
     }
 
     @PostMapping("/remove")
-    public String remove(Long tno,RedirectAttributes redirectAttributes) {
+    public String remove(Long tno,PageRequestDTO pageRequestDTO, RedirectAttributes redirectAttributes) {
         log.info("==========remove==========");
         log.info("tno: " + tno);
 
         todoService.remove(tno);
+
+        redirectAttributes.addAttribute("page",1);
+        redirectAttributes.addAttribute("size",pageRequestDTO.getSize());
 
         return "redirect:/todo/list";
 
     }
 
     @PostMapping("/modify")
-    public String modify(@Valid TodoDTO todoDTO,
+    public String modify(PageRequestDTO pageRequestDTO,
+                         @Valid TodoDTO todoDTO,
                         BindingResult bindingResult,
                          RedirectAttributes redirectAttributes)
      {
          if(bindingResult.hasErrors()) {
              log.info("has errors............");
              redirectAttributes.addFlashAttribute("errors",bindingResult.getAllErrors());
-             redirectAttributes.addAttribute("tno",todoDTO.getTno());
+
+             // ★ tno뿐만 아니라 page와 size도 같이 넘겨줘야 주소창에 유지됩니다.
+
              return "redirect:/todo/modify";
          }
          log.info(todoDTO);
          todoService.modify(todoDTO);
+
+         redirectAttributes.addAttribute("page",pageRequestDTO.getPage());
+         redirectAttributes.addAttribute("size",pageRequestDTO.getSize());
+
          return "redirect:/todo/list";
 
     }
--- src/main/webapp/WEB-INF/views/todo/modify.jsp
+++ src/main/webapp/WEB-INF/views/todo/modify.jsp
@@ -48,6 +48,9 @@
 
                 <div class="card-body">
                 <form action="/todo/modify" method="post">
+                <input type="hidden" name="page" value="${pageRequestDTO.page}">
+                <input type="hidden" name="size" value="${pageRequestDTO.size}">
+
 
                          <div class="input-group mb-3">
                          <span class="input-group-text">TNO</span>
@@ -127,7 +130,7 @@ document.querySelector(".btn-secondary").addEventListener("click",function(e) {
     e.preventDefault()
     e.stopPropagation()
 
-    self.location="/todo/list"
+    self.location=`/todo/list?${pageRequestDTO.link}`
     },false);
 
    </script>
